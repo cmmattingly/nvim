9293546243d1e5e769d92c7e5bed662a38c304e2
cmmattingly <chase.mattingly68@gmail.com>
2024-06-26 14:16:42 -0400

    remove lazy from telescope

--- lua/cfig/lazy/telescope.lua
+++ lua/cfig/lazy/telescope.lua
@@ -1,6 +1,5 @@
 return {
     "nvim-telescope/telescope.nvim",
-    lazy = true, 
     tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -10,6 +9,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
