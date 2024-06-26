76b5b090e0bbbba0cfa73e4769cee35d9e7cddba
cmmattingly <chase.mattingly68@gmail.com>
2024-06-26 17:27:20 -0400

    harpoon + telescope fixes

--- lua/cfig/lazy/harpoon.lua
+++ lua/cfig/lazy/harpoon.lua
@@ -1,15 +1,71 @@
 return {
+  {
     "theprimeagen/harpoon",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    config=function()
-        require("harpoon"):setup()
+    branch = "harpoon2",
+    lazy = false,
+    requires = { "nvim-lua/plenary.nvim" }, -- if harpoon requires this
+    config = function()
+      require("harpoon").setup({})
+
+      local function toggle_telescope_with_harpoon(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = require("telescope.config").values.file_previewer({}),
+            sorter = require("telescope.config").values.generic_sorter({}),
+          })
+          :find()
+      end
+      vim.keymap.set("n", "<leader>h", function()
+        local harpoon = require("harpoon")
+        toggle_telescope_with_harpoon(harpoon:list())
+      end, { desc = "Open harpoon window" })
     end,
     keys = {
-		{ "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-		{ "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
-		{ "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
-		{ "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
-	}
+      {
+        "<leader>ha",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "harpoon file",
+      },
+      {
+        "<C-b>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon quick menu",
+      },
+      {
+        "<leader>h1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "harpoon to file 1",
+      },
+      {
+        "<leader>h2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "harpoon to file 2",
+      },
+      {
+        "<leader>h3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "harpoon to file 3",
+      },
+    },
+  },
 }
