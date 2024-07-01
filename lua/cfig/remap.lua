8fa1272ddbae1f373385a0788a42e9c7a3988141
cmmattingly <chase.mattingly68@gmail.com>
2024-07-01 00:32:03 -0400

    remap for pasting

--- lua/cfig/remap.lua
+++ lua/cfig/remap.lua
@@ -7,3 +7,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste without losing paste registry from delete
+vim.keymap.set("n", "<leader>p", "\"_dP")
