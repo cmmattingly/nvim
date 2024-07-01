fb4e5dbfcde795796943e95d0ed881ea0dba64db
cmmattingly <chase.mattingly68@gmail.com>
2024-07-01 13:59:34 -0400

    move cursor to 0 index when scrolling

--- lua/cfig/remap.lua
+++ lua/cfig/remap.lua
@@ -3,8 +3,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- center 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>0zz")
+vim.keymap.set("n", "<C-u>", "<C-u>0zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
