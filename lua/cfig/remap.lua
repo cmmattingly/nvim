da599edc193127ee637faa841ab169c20d663fc4
cmmattingly <chase.mattingly68@gmail.com>
2024-07-01 00:16:36 -0400

    remaps for centering while searching

--- lua/cfig/remap.lua
+++ lua/cfig/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- center 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
